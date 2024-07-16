cc5e43a171c72e44ed03fd72c780361a809fbf30
ifocabot <77793836+ifocabot@users.noreply.github.com>
2024-07-16 22:00:00 +0700

    clock-out.spec.ts

--- tests/clock-out.spec.ts
+++ tests/clock-out.spec.ts
@@ -5,6 +5,6 @@ test("Clock Out", async ({ page }) => {
   if (skip()) return;
   await goToLiveAttendance(page);
 
-  await page.getByText("Clock Out", { exact: true }).click();
+  await page.getByRole('button', { name: 'Clock Out' }).click();
   await expect(page.getByText("Successfully clock out")).toBeVisible();
 });
